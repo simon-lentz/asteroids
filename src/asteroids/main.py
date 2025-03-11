536feba007de2bc924f02c4b82306abb7af5d43d
simon-lentz <slentz445@gmail.com>
2025-03-11 11:50:39 -0400

    Adds screen cap.

--- src/asteroids/main.py
+++ src/asteroids/main.py
@@ -11,8 +11,20 @@ def main():
     print(
         f"Starting Asteroids!\nScreen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}"
     )
+
+    # initialize pygame
     pygame.init()
+
+    # set screen using constants.py
+    pygame.display.set_caption("Asteroids!")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    # create a game clock to decouple game speed (should be linked to FPS) from while loop refresh rate
+    clock = pygame.time.Clock()
+
+    # delta time tracking variable in seconds
+    dt_s = 0
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -20,6 +32,8 @@ def main():
 
         pygame.Surface.fill(screen, (0, 0, 0))
         pygame.display.flip()
+        dt_ms = clock.tick()
+        dt_s = dt_ms / 1000
 
 
 if __name__ == "__main__":
